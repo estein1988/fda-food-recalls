f375edf364639c3da09c876fd9e404eff3687f8f
estein1988 <estein1988@gmail.com>
2020-09-09 14:40:43 -0600

    Major upgrades.

--- food_recall_project/config/routes.rb
+++ food_recall_project/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :recalls
   get 'state', to: 'recalls#filterByState'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'recalling_firm', to: 'recalls#filterByCompany'
+  get 'recall_number', to: 'recalls#filterByRecall'
 end
