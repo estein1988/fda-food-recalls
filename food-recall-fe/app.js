790a7700120fdd6393e077cdf8d66e4db66731cc
estein1988 <estein1988@gmail.com>
2020-09-10 16:42:32 -0600

    Add search functionality.

--- food-recall-fe/app.js
+++ food-recall-fe/app.js
@@ -1,29 +1,31 @@
+const searchParams = new URLSearchParams(window.location.search)
+const search_term = searchParams.get("search_term")
+let url = 'http://localhost:3000/recalls'
+
+const formResults = document.querySelector('#formresults')
+
+if (search_term) {
+    url = `${url}?search_term=${search_term}`
+    fetchRecalls()
+}
+function fetchRecalls() {
+    fetch(url)
+    .then(response => response.json())
+    .then(handleRecalls)
+}
+function handleRecalls(recalls) {
+    recalls.forEach(recall => {
+        const recallCard = document.createElement('div')
+        const recallDescription = document.createElement('h2')
+        const recallingFirm = document.createElement('h3')
+        recallDescription.innerHTML = `<a href='show_card.html?recall_number=${recall.recall_number}'>${recall.recall_number}</a>`
+        recallingFirm.textContent = recall.recalling_firm
+        formResults.appendChild(recallCard)
+        recallCard.append(recallDescription, recallingFirm)
+    })
+}
+
 const mainList = document.getElementById('stateList')
-
-// const states = results.state
-
-// function renderList(states) {
-//     heroines.forEach(heroine => {
-//         const name = document.createElement('h3')
-//         const super_name = document.createElement('li')
-
-//         name.innerHTML = `<a href='show.html?id=${heroine.id}'>${heroine.name}</a>`
-//         super_name.textContent = heroine.super_name
-
-//         mainList.append(name, super_name)
-//     })
-// }
-
-// fetch('http://localhost:3000/recalls')
-//     .then(response => response.json())
-//     .then(recalls => {
-//         const stateDropdown = document.querySelector('#state-field')
-//         recalls.state.forEach(state => {
-//             const stateOption = document.createElement('option')
-//             stateOption.textContent = recall.state
-//             stateOption.value = recall.state
-//             stateDropdown.appendChild(stateOption)
-//         })
 
 let states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
@@ -38,11 +40,3 @@ function populateStates(states) {
 
 populateStates(states)
 
-
-
-//this code isn't doing anything, the form submittal is still working without it:
-// fetch('http://localhost:3000/users')
-//     .then(response => response.json())
-//     .then(users => {
-//         const usernameField = document.querySelector('#username-field')
-//     })
